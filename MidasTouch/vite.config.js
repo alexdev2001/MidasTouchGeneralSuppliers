d379eebb756d1b33dfbc19a8ad2bf707598df037
alexdev2001 <aleximani201@gmail.com>
2024-07-04 16:01:35 +0200

    last trick

--- MidasTouch/vite.config.js
+++ MidasTouch/vite.config.js
@@ -3,6 +3,14 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        'firebase/app',
+        'firebase/firestore'
+      ]
+    }
+  },
   optimizeDeps: {
     include: [
       'firebase/app',
